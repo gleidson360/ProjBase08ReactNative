45971a04ebf21171f53f0f46a50651705f4677d4
Gleidson Medrado <gleidsonmedrado@hotmail.com>
2024-03-15 13:26:29 -0300

    Adicionando Hooks

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Text, StatusBar, Pressable } from "react-native";
+
+import { LinearGradient } from "expo-linear-gradient";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 export default function App() {
+  const [escaneado, estadoEscaneado] = useState(false);
+
+  async function ObterPermissao() {
+    await BarCodeScanner.requestPermissionsAsync();
+  }
+
+  function Escanear({ type, data }) {
+    estadoEscaneado(true);
+    alert(data);
+  }
+
+  useEffect(function () {
+    ObterPermissao();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient colors={["#232526", "#414345"]}>
+      <StatusBar
+        translucent
+        barStyle="light-content"
+        backgroundColor="transparent"
+      />
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
